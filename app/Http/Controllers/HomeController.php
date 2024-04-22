b8e87edbe5ed2bb07e24b197c7c0a24eca169ea5
aymen <devaymen15.5.3@gmail.com>
2024-04-22 05:56:21 +0100

    started usnig model an migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\EventsModel;
+use App\Repositories\VersionRepo;
 use http\Env\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -13,18 +13,21 @@ class HomeController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    private EventsModel $model;
+    protected $repo;
+
+    public function __construct(VersionRepo $versionRepo){
+        $this->repo = $versionRepo;
+    }
 
     public function index(): view
     {
         $endDate = null;
         $category = 'all';
-        $this->model = new EventsModel();
         if (isset($_POST['filter'])) {
             if (isset($_POST['endDate'])) $endDate = $_POST['endDate'];
             if (isset($_POST['category'])) $category = $_POST['category'];
         }
-        $data = $this->model->getAllEvents(date('Y-m-d'), $endDate, $category);
+        $data = $this->repo->getAll();
 
         return view('home')->with('data', $data);
     }
---
+++ app/repository/VersionRepo.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Repositories;
+
+use App\Models\VERSION;
+use App\Models\EVENMENT;
+
+class VersionRepo
+{
+    protected $model;
+    function __construct(VERSION $model){
+        $this->model = $model;
+    }
+
+    function getAll($params = null){
+        $query = $this->model::query();
+        $query->where('DATE', '>=', date('Y-m-d'));
+        // if (!empty($params['endDate'])) {
+        //     $query->where('DATE', '<=', $params['endDate']);
+        // }
+        return $this->model::select($query);
+    }
+    
+}
